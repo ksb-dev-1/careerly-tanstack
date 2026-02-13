3d51848a3b5567842662202f5aca1739cbf18059
ksb-dev-1 <ksbabaleshwar@gmail.com>
2026-02-13 14:11:20 +0530

    initial commit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,4 +6,9 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  // If your application runs on a port other than 3000, you must add it to the trustedOrigins
+  //   trustedOrigins: ["http://localhost:3001"],
 });
