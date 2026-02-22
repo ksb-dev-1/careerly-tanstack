68c637d25b613abd2aa6d543663eeae5bfc5615a
ksb-dev-1 <ksbabaleshwar@gmail.com>
2026-02-23 01:06:48 +0530

    initial commit

--- src/components/home/start-exploring-button.tsx
+++ src/components/home/start-exploring-button.tsx
@@ -1,46 +1,11 @@
-"use client";
-
-import { useEffect } from "react";
-
 import { CustomLink } from "@/components/shared/custom-link";
-import { Skeleton } from "@/components/ui/skeleton";
-import { UserRole } from "@/generated/prisma/browser";
-import { useCustomRouter } from "@/hooks/useCustomRouter";
-import { authClient } from "@/lib/auth-client";
-import { EMPLOYER_ROUTES, JOB_SEEKER_ROUTES, ROUTES } from "@/lib/routes";
+import { ROUTES } from "@/lib/routes";
 
 export function StartExploringButton() {
-  const { data: session, isPending } = authClient.useSession();
-  const router = useCustomRouter();
-
-  useEffect(() => {
-    if (session?.user.id && !session.user.role) {
-      router.push(ROUTES.SELECT_USER_ROLE);
-    }
-  }, [session, router]);
-
-  if (isPending) {
-    return (
-      <Skeleton className="border text-transparent rounded-full h-14 px-6 font-medium">
-        Start Exploring
-      </Skeleton>
-    );
-  }
-
-  let href: string = ROUTES.SIGN_IN;
-
-  if (session?.user.role === UserRole.JOB_SEEKER) {
-    href = JOB_SEEKER_ROUTES.JOBS;
-  }
-
-  if (session?.user.role === UserRole.EMPLOYER) {
-    href = EMPLOYER_ROUTES.JOBS;
-  }
-
   return (
     <div className="flex items-center">
       <CustomLink
-        href={href}
+        href={ROUTES.SIGN_IN}
         className="bg-brand rounded-full px-6 py-4 hover:bg-brand-hover transition text-white dark:text-background font-semibold"
         prefetch
       >
